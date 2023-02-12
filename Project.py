4832d927a42f71c49efb7746b06ee6d0bd5102ef
Zarna Bankatwala <103313632+Zarna-99@users.noreply.github.com>
2023-02-12 16:37:10 +0530

    Update Project.py

--- Project.py
+++ Project.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-#import matplotlib as plt
+import matplotlib as plt
 import seaborn as sns
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 
 import plotly.graph_objects as go
