65bab69c1c8d5343df021e5900fa94b37243b8e8
Zarna Bankatwala <103313632+Zarna-99@users.noreply.github.com>
2023-02-16 15:08:22 +0530

    Update Project_Final.py

--- Project_Final.py
+++ Project_Final.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import plotly.express as px
